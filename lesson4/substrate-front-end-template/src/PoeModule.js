ae641873e95d0e10eb18f4da3b832fc668bab40b
Lavande <admin@5dimn.com>
2020-06-21 01:08:38 +0800

    finished homework 4

--- lesson4/substrate-front-end-template/src/PoeModule.js
+++ lesson4/substrate-front-end-template/src/PoeModule.js
@@ -11,16 +11,39 @@ function Main (props) {
 
   // The transaction submission status
   const [status, setStatus] = useState('');
+  const [queryInfo, setQueryInfo] = useState('');
   const [digest, setDigest] = useState('');
   const [owner, setOwner] = useState('');
-  const [dest, setDest] = useState('');
+  const [user, setUser] = useState('');
   const [blockNumber, setBlockNumber] = useState(0);
+  const [memo, setMemo] = useState('');
+
+  function hex_to_ascii(str1) {
+    var hex  = str1.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+      str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
+  }
 
   useEffect(() => {
     let unsubscribe;
     api.query.poeModule.proofs(digest, (result) => {
-      setOwner(result[0].toString());
-      setBlockNumber(result[1].toNumber());
+      setOwner(result[1].toString());
+      setBlockNumber(result[2].toNumber());
+    }).then(unsub => {
+      unsubscribe = unsub;
+    })
+      .catch(console.error);
+
+    return () => unsubscribe && unsubscribe();
+  }, [digest, api.query.PoeModule]);
+
+  useEffect(() => {
+    let unsubscribe;
+    api.query.poeModule.proofByAcct(user, (result) => {
+      setQueryInfo(result[0].toJSON());
     }).then(unsub => {
       unsubscribe = unsub;
     })
@@ -59,6 +82,14 @@ function Main (props) {
           />
         </Form.Field>
 
+        <Input
+          label='Memo'
+          placeholder='(no longer than 256 bytes)...'
+          state='memo'
+          type='string'
+          onChange={(_, { value }) => setMemo(value)}
+        />
+
         <Form.Field>
           <TxButton
             accountPair = {accountPair}
@@ -68,48 +99,45 @@ function Main (props) {
             attrs = {{
               palletRpc: 'poeModule',
               callable: 'createClaim',
-              inputParams: [digest],
-              paramFields: [true]
-            }}
-          />
-
-          <TxButton
-            accountPair={accountPair}
-            label='Revoke Claim'
-            setStatus={setStatus}
-            type='SIGNED-TX'
-            attrs={{
-              palletRpc: 'poeModule',
-              callable: 'revokeClaim',
-              inputParams: [digest],
-              paramFields: [true]
-            }}
-          />
-
-          <Input
-            label='Destination'
-            placeholder='Input the account of the receiver'
-            state='dest'
-            type='string'
-            onChange={(_, { value }) => setDest(value)}
-          />
-
-          <TxButton
-            accountPair={accountPair}
-            label='Transfer Claim'
-            setStatus={setStatus}
-            type='SIGNED-TX'
-            attrs={{
-              palletRpc: 'poeModule',
-              callable: 'transferClaim',
-              inputParams: [digest, dest],
+              inputParams: [digest, memo],
               paramFields: [true]
             }}
           />
         </Form.Field>
 
         <div>{status}</div>
-          <div>{`Claim Info. owner: ${owner}, blockNumber: ${blockNumber}`}</div>
+          <div>{`You have successfully claimed a file (Hash: ${digest}; Memo: ${memo}) in block ${blockNumber}.`}</div>
+      </Form>
+
+      <h1>Proof of Existence - User Info</h1>
+      <Form>
+
+        <Form.Field>
+
+          <Input
+            label='User Address'
+            placeholder='0x123abc...'
+            state='user'
+            type='string'
+            onChange={(_, { value }) => setUser(value)}
+          />
+
+          <TxButton
+            accountPair={accountPair}
+            label='Query user doc'
+            setStatus={setQueryInfo}
+            type='QUERY'
+            attrs={{
+              palletRpc: 'poeModule',
+              callable: 'proofByAcct',
+              inputParams: [user],
+              paramFields: [true]
+            }}
+          />
+
+        </Form.Field>
+
+        <div id="docinfo">{`Doc Info. ${queryInfo}`}</div>
       </Form>
 
     </Grid.Column>
