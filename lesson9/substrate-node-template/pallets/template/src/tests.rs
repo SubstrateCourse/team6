6b301fc2e866bb6af2189b900adea8bdffd94fdf
AmadeusGB <1069830494@qq.com>
2020-07-12 21:43:38 +0800

    homework for lesson9

--- lesson9/substrate-node-template/pallets/template/src/tests.rs
+++ lesson9/substrate-node-template/pallets/template/src/tests.rs
@@ -5,8 +5,22 @@ use frame_support::{assert_ok, assert_noop};
 
 #[test]
 fn test_onchain() {
-	new_test_ext().execute_with(|| {
-		// Test onchain logic here
+	let (mut t, _pool_state, _offchain_state) = ExtBuilder::build();
+	t.execute_with(|| {
+		let price = 9227;
+		let value = [ 9227 ];
+		let acct: <Test as system::Trait>::AccountId = Default::default();
+
+		// when `save_number` is being called
+		assert_ok!(TemplateModule::save_number(Origin::signed(acct), price));
+
+		// added to storage
+		assert_eq!(<Prices>::get(), value);
+
+		// an event is emitted
+		let expected_event = TestEvent::template(RawEvent::NewPrice(acct, price));
+		assert!( System::events().iter().any(|er| er.event == expected_event) );
+
 	});
 }
 
